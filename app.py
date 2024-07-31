1a4ad858e02011025a1f02f1f6c783b034cc08a1
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-31 11:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,5 +173,5 @@ def placement():
 
 # ========================python main===================================================
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
 
-    app.run(debug=True)
